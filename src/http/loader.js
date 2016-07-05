37fe86c53ca3b76f5cf2fd2c8513e67bfeb5aa58
moonwalker <dorin.tufar@freshcells.de>
2016-07-05 20:47:35 +0200

    remove http layer

--- src/http/loader.js
+++ src/http/loader.js
@@ -1,9 +1,7 @@
 module.exports = function(app, argv) {
-    var http = require('./http.js')(app, argv);
     var symbiosis = require('./symbiosis.js')(argv);
     
     return {
-        "http" : http,
         "symbiosis" : symbiosis
     }
 };
