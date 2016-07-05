e44f91834be8cc85e4285e7c77c06ce60d0cc6fb
moonwalker <dorin.tufar@freshcells.de>
2016-07-05 20:44:37 +0200

    remove http layer

--- src/http/loader.js
+++ src/http/loader.js
@@ -1,7 +1,9 @@
-module.exports = function(argv) {
+module.exports = function(app, argv) {
+    var http = require('./http.js')(app, argv);
     var symbiosis = require('./symbiosis.js')(argv);
     
     return {
+        "http" : http,
         "symbiosis" : symbiosis
     }
 };
--- src/http/symbiosis.js
+++ src/http/symbiosis.js
@@ -2,10 +2,7 @@ var symbiosis = require("symbiosis")("config/maps/symbiosis_entities.json");
 
 module.exports = function(argv) {
     var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || argv.port;
-    var host = process.env.OPENSHIFT_NODEJS_IP || argv.host || "127.0.0.1";
-    var io = symbiosis.getIo().listen(port, host, function(){
-        console.log("✔ Symbiosis listening at %s:%d ", host, port);
-    });
+    var io = symbiosis.getIo().listen(port);
     
     io.on('connect', function(socket) {
         var headers = socket.handshake.headers;
